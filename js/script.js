fc2287857719057324c5418bb950880c764e250d
Zahra <zahra.ghasemian78@gmail.com>
2023-09-18 13:31:04 +0330

    edit buttom

--- js/script.js
+++ js/script.js
@@ -7,4 +7,44 @@ $(function () {
    $("#sidebar").on("show.bs.collapse", function () {
      $("#main-content").addClass("col-md-10");
    });
+
+
+var ch1 = $('#chart1');
+var chart1 = new chart(ch1, {
+  type: "line",
+  data: {
+    lable: [
+      "شنبه",
+      "یکشنبه",
+      "دو شنبه",
+      "سه شنبه",
+      "چهار شنبه",
+      "پنجشنبه",
+      "جمعه",
+    ],
+
+    datasets: [
+      {
+        data: [12000, 11500, 13000, 14000, 13500, 15000, 14500],
+        backgroundColor: "#fff",
+        borderColor: "#fff",
+        pointBackgroundColor: "#fff",
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [{
+          display: false
+        }],
+      xAxes: [{
+          display: false,
+        }]
+    },
+    legend:{
+      display:false
+    }
+  },
+});
+
 });
